440460fe76e8743ea9efd4fb05aa16aa9e5bee85
alingogo <alin.tottori@gmail.com>
2012-11-18 14:18:11 +0900

    add omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Tennis::Application.routes.draw do
+  resources :posts
+
+  root :to => "posts#index"
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
