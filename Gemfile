177533376569b0d79e3152c7f7f03731b556d4e3
alin <alin@localhost.localdomain>
2012-12-05 04:30:16 +0900

    a

--- Gemfile
+++ Gemfile
@@ -15,8 +15,6 @@ group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
 
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platforms => :ruby
 
   gem 'uglifier', '>= 1.0.3'
 end
@@ -44,6 +42,8 @@ end
 # gem 'debugger'
 group :development, :test do
   gem 'sqlite3'
+  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
+  gem 'therubyracer', '0.10.2', :platforms => :ruby
 end
 
 group :test do
--- db/schema.rb
+++ db/schema.rb
@@ -24,20 +24,20 @@ ActiveRecord::Schema.define(:version => 20121118045257) do
   add_index "posts", ["user_id"], :name => "index_posts_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                               :default => "", :null => false
+    t.string   "encrypted_password",                  :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",                       :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                                          :null => false
+    t.datetime "updated_at",                                          :null => false
     t.string   "name"
-    t.integer  "uid"
+    t.integer  "uid",                    :limit => 8
     t.string   "provider"
   end
 
