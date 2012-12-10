621cb860d545ca144b5025da129a3e0beb783a87
alingogo <alin.tottori@gmail.com>
2012-12-11 01:00:04 +0900

    change facebook set with issue #1

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -207,7 +207,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   # require "omniauth-facebook"
-  config.omniauth :facebook, "519139171432716", "831f0aff7aa979cbc39d61bf753e29da"
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
